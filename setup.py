40f2fbd6fec96d9143d53c35ddb4f15635336756
mitchell-dawson <mitchellgdawson@gmail.com>
2022-11-18 18:05:12 +0100

    build: added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="GeneticAlgorithmsWithPython",
+    version="0.0.1",
+    author="Mitchell Dawson",
+    description="Package for genetic algorithms with Python",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+)
